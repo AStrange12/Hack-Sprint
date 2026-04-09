a399e202d50ea9a9262649de97d29ff8f12b0758
AStrange12 <85741262+AStrange12@users.noreply.github.com>
2026-04-09 18:41:25 +0530

    Create config.ts

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "apiKey": "AIzaSyASpy2h3rmMTyNQBwA_1OWowHru6_Taa6Q",
+  "authDomain": "studio-4901046486-e5c2f.firebaseapp.com",
+  "projectId": "studio-4901046486-e5c2f",
+  "storageBucket": "studio-4901046486-e5c2f.firebasestorage.app",
+  "messagingSenderId": "320145439182",
+  "appId": "1:320145439182:web:eae9fb85154154fa11539f"
+};
